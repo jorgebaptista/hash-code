63a40264ca29637e3b1c80cf312ce889dd5a6e39
Jorge Baptista <jorge-baptista@outlook.com>
2020-03-08 22:15:28 +0000

    Problem solved but still needs optimizing

--- Source/2020/Solution/Program.cs
+++ Source/2020/Solution/Program.cs
@@ -7,117 +7,131 @@ namespace Solution
     {
         public struct Book
         {
-            public int id;
             public int score;
             public bool scanned;
         }
 
         public struct Library
         {
-            public int nBooks;
+            public int totalBooks;
             public int signupTime;
             public int booksPerDay;
             public int[] bookSet;
-            public int[] totalScore;
-            public bool signingUp;
+            // public bool signingUp;
             public bool signedUp;
+            public int signedUpID;
+        }
 
-            public int nScannedBooks;
+        public struct SignedLibrary
+        {
+            public int id;
             public List<int> scannedBooks;
         }
 
-        // public struct SignedLibrary
-        // {
-        //     public int[]
-        // }
-
         static void Main(string[] args)
         {
-            string[] input = System.IO.File.ReadAllLines(@"..\Input\a_example.txt");
-            string[] line0 = input[0].Split(' ');
-            Book[] books = new Book[Int32.Parse(line0[0])];
-            string[] bookScores = input[1].Split(' ');
+            #region Input read and variable assignment
 
+            // Read the input file
+            string[] input = System.IO.File.ReadAllLines(@"..\Input\a_example.txt");
+
+            // Separate first line by spaces and convert all array elements to Int for future reference
+            int[] line0 = Array.ConvertAll(input[0].Split(' '), int.Parse);
+            // Create B total Books based on the first number of the first line of the input file
+            Book[] books = new Book[line0[0]];
+            // Same for Libraries
+            Library[] libraries = new Library[line0[1]];
+            // And days
+            int maxDays = line0[2];
+
+            // Separate second line by spaces and convert all array elements to Int for future reference
+            int[] line1 = Array.ConvertAll(input[1].Split(' '), int.Parse);
+            // Iterate through all the books by Id and assign scores to each from the input line 2
             for (int i = 0; i < books.Length; i++)
             {
-                books[i].id = i;
-                books[i].score = Int32.Parse(bookScores[i]);
+                books[i].score = line1[i];
+                // Set scanned status to false because no books were scanned yet
                 books[i].scanned = false;
             }
 
-            Library[] libraries = new Library[Int32.Parse(line0[1])];
-
+            // Iterate through each Library
             for (int i = 0; i < libraries.Length; i++)
             {
-                string[] libraryData = input[(i + 1) * 2].Split(' ');
-                libraries[i].nBooks = Int32.Parse(libraryData[0]);
-                libraries[i].signupTime = Int32.Parse(libraryData[1]);
-                libraries[i].booksPerDay = Int32.Parse(libraryData[2]);
+                // get the 3rd line in the input file and then go 2 by 2 for each library id and assign it to an array converted to Int
+                int[] libraryData2 = Array.ConvertAll(input[(i + 1) * 2].Split(' '), int.Parse);
+
+                // Assign each number of the first line: total books of this library, signup time and how many books per day it can scan
+                libraries[i].totalBooks = libraryData2[0];
+                libraries[i].signupTime = libraryData2[1];
+                libraries[i].booksPerDay = libraryData2[2];
+
+                // get the 4rd line in the input file and then go 2 by 2 for each library and assign it to the library bookset
                 libraries[i].bookSet = Array.ConvertAll(input[((i + 1) * 2) + 1].Split(' '), int.Parse);
 
-                libraries[i].signingUp = false;
+                // Set signingUp and signedUp as false as default value
+                // libraries[i].signingUp = false;
                 libraries[i].signedUp = false;
             }
 
-            int maxDays = int.Parse(line0[2]);
-
+            // Boolean to check if any library is currently being signed up
             bool signingUp = false;
+            // Keep check of how many libraries has been signed in total            
+            // int totalSignedLibraries = 0;
 
             int signingTimeLeft = 0;
+            int signingUpLibraryID = 0;
 
-            int signedLibraries = 0;
+            SignedLibrary[] signedLibraries = new SignedLibrary[0];
+
+            #endregion
+
 
             for (int day = 0; day < maxDays; day++)
             {
-                if (signingUp && signingTimeLeft > 0)
-                {
-                    signingTimeLeft--;
-                }
-                else if (signingUp && signingTimeLeft == 0)
-                {
-                    signedLibraries++;
-                    
-                    for (int i = 0; i < libraries.Length; i++)
-                    {
-                        if (libraries[i].signingUp)
-                        {
-                            signingUp = false;
-                            libraries[i].signingUp = false;
-                            libraries[i].signedUp = true;
-                        }
-                    }
-                }
-
-                else if (!signingUp)
+                if (!signingUp)
                 {
                     for (int i = 0; i < libraries.Length; i++)
                     {
                         if (!libraries[i].signedUp)
                         {
-                            libraries[i].signingUp = true;
                             signingUp = true;
-
                             signingTimeLeft = libraries[i].signupTime;
+                            signingUpLibraryID = i;
                         }
                     }
                 }
-
+                else if (signingTimeLeft > 0) signingTimeLeft--;
                 else
                 {
-                    for (int i = 0; i < libraries.Length; i++)
+                    signingUp = false;
+                    libraries[signingUpLibraryID].signedUp = true;
+
+                    Array.Resize(ref signedLibraries, signedLibraries.Length + 1);
+
+                    signedLibraries[signedLibraries.Length - 1].id = signingUpLibraryID;
+                    libraries[signingUpLibraryID].signedUpID = signedLibraries.Length - 1;
+
+                    signedLibraries[signedLibraries.Length - 1].scannedBooks = new List<int>();
+                }
+
+                for (int signedLibrary = 0; signedLibrary < signedLibraries.Length; signedLibrary++)
+                {
+                    int currentLibraryID = signedLibraries[signedLibrary].id;
+
+                    if (signedLibraries[signedLibrary].scannedBooks.Count == libraries[currentLibraryID].totalBooks) break;
+                    else
                     {
-                        if (libraries[i].signedUp)
+                        for (int scannedBooks = 0; scannedBooks < libraries[currentLibraryID].booksPerDay; scannedBooks++)
                         {
-                            for (int j = 0; j < libraries[i].bookSet.Length; j++)
+                            for (int book = 0; book < libraries[currentLibraryID].totalBooks; book++)
                             {
-                                for (int k = 0; k < libraries[i].nBooks; k++)
+                                int currentBookID = libraries[currentLibraryID].bookSet[book];
+
+                                if (!books[currentBookID].scanned)
                                 {
-                                    if (!books[libraries[i].bookSet[j]].scanned)
-                                    {
-                                        books[libraries[i].bookSet[j]].scanned = true;
-                                        libraries[i].nScannedBooks++;
-                                        libraries[i].scannedBooks.Add(books[libraries[i].bookSet[j]].id);
-                                    }
+                                    books[currentBookID].scanned = true;
+
+                                    signedLibraries[signedLibrary].scannedBooks.Add(currentBookID);
                                 }
                             }
                         }
@@ -125,12 +139,22 @@ namespace Solution
                 }
             }
 
-            int[] line1;
+            List<string> libraryData = new List<string>();
 
-            string[] lines = {signedLibraries.ToString(),};
+            libraryData.Add(signedLibraries.Length.ToString());
 
-            Console.WriteLine(signedLibraries);
-            Console.WriteLine()
+            for (int i = 0; i < signedLibraries.Length; i++)
+            {
+                libraryData.Add(signedLibraries[i].id.ToString() + " " + signedLibraries[i].scannedBooks.Count.ToString());
+                libraryData.Add(String.Join(" ", signedLibraries[i].scannedBooks));
+            }
+
+            foreach (string item in libraryData)
+            {
+                Console.WriteLine(item);
+            }
+
+            System.IO.File.WriteAllLines(@"A.output", libraryData);
         }
     }
 }
